94e73a8a58acb262e6d55af135a8999e2cb2c126
SharanyaGaneshPrasad <sharanya.ganeshprasad@digitalcareerinstitute.org>
2024-11-20 16:49:50 +0100

    change in folder structure

---
+++ apply-terraform.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# fail on any error
+set -eu
+
+# initialize terraform
+terraform init
+
+# # apply terraform
+terraform apply -auto-approve
+
+# destroy terraform
+# terraform destroy -auto-approve
